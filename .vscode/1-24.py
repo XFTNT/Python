75ad0a01ece26ce264ec8d1e464f0655df2bf02d
LIU YIFEI <13733702369@163.com>
2025-01-25 15:41:41 +0800

    修复代码格式并添加新的 Python 文件

---
+++ .vscode/1-24.py
@@ -0,0 +1,14 @@
+# def sum(a, b):
+#     c = a + b
+#     print(c)
+#     pass
+# sum(3, 4)
+
+# def sum(*nums):
+#     sum = 0
+#     for num in nums:
+#         sum += num
+#         pass
+#     return sum
+#     pass
+# print(sum(3, 4, 5, 8, 12))
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 print('hellow world!')
 a = 3
-b= 4
+b = 4
 print(a)
 print(b) 
+saSashjAS = 54
